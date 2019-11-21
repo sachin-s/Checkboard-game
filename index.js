4c2dec164a59363bd4d017211684ccdf1d07f0b6
Sachin S <sachins1221@gmail.com>
2019-11-21 20:46:04 +0530

    bottom check added

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ var colNum = 0;
 var rowNum = 0;
 $('#message').text(player1+' turn to play(red color)');
 
+function checkBottom(col)
+{
+    for(i=6;i>-1;i--)
+    {
+        if($('#checkboard').children().find('tr').eq(i).find('td').eq(col).find('button').css('background-color')==="rgb(221, 221, 221)")
+        {
+            return i;
+        }
+    }
+}
+
 $('td').click(function(){
     colNum = $(this).parent().children().index($(this));
     rowNum = $(this).parent().parent().children().index($(this).parent());
@@ -18,10 +29,14 @@ $('td').click(function(){
     if(currentPlayer===1)
     {
         currentPlayer *=-1;
+        rowNum = checkBottom(colNum);
+        console.log("rowNum : "+rowNum);
         $('#checkboard').children().find('tr').eq(rowNum).find('td').eq(colNum).find('button').css('background-color',player1Color);
         $('#message').text(player2+' turn to play(blue color)');
     }else{
         currentPlayer *=-1;
+        rowNum = checkBottom(colNum);
+        console.log("rowNum : "+rowNum);
         $('#checkboard').children().find('tr').eq(rowNum).find('td').eq(colNum).find('button').css('background-color',player2Color);
         $('#message').text(player1+' turn to play(red color)');
     }
